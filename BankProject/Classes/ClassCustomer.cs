e7cdbc4d11b88d3ddd0e3a7ccf6f37961be9cfaa
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-12-12 11:02:30 -0700

    fix: adjusted sql table column names to camelCase

--- BankProject/Classes/ClassCustomer.cs
+++ BankProject/Classes/ClassCustomer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Net.Mail;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace BankProject.Classes
 {
@@ -12,48 +16,45 @@ namespace BankProject.Classes
         public ClassEmployee? FinancialAdvisor { get; set; }
         public ClassBranch? Branch { get; set; }
         public List<ClassAbstractAccount>? MyListAccounts { get; set; }
-        int customerID { get; set; }
-        string firstName { get; set; }
-        string lastName { get; set; }
-        DateOnly dateOfBirth { get; set; }
-        ClassDocument document { get; set; }
-        ClassAddress address { get; set; }
-        string phoneNumber { get; set; }
-        string emailAddress { get; set; }
+        int CustomerId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateOnly DateOfBirth { get; set; }
+        ClassDocument Document { get; set; }
+        ClassAddress Address { get; set; }
+        string PhoneNumber { get; set; }
+        string EmailAddress { get; set; }
 
-        public ClassCustomer(int customerID, string firstName, string lastName, DateOnly dateOfBirth, ClassDocument document, ClassAddress address, string phoneNumber, string emailAddress, ClassEmployee? financialAdvisor, ClassBranch? branch, List<ClassAbstractAccount>? myListAccounts)
+        public ClassCustomer(int customerId, string firstName, string lastName, DateOnly dateOfBirth, ClassDocument document, ClassAddress address, string phoneNumber, string emailAddress, ClassEmployee? financialAdvisor, ClassBranch? branch, List<ClassAbstractAccount>? myListAccounts)
         {
-            this.customerID = customerID;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.dateOfBirth = dateOfBirth;
-            this.document = document;
-            this.address = address;
-            this.phoneNumber = phoneNumber;
-            this.emailAddress = emailAddress;
+            CustomerId = customerId;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Document = document;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            EmailAddress = emailAddress;
             FinancialAdvisor = financialAdvisor;
             Branch = branch;
             MyListAccounts = myListAccounts;
         }
 
 
-
-        //public ClassCustomer(int customerID, string firstName, string lastName, DateOnly dateOfBirth, ClassDocument document, ClassAddress address, string phoneNumber, string emailAddress)
-        //{
-        //    this.customerID = customerID;
-        //    this.firstName = firstName;
-        //    this.lastName = lastName;
-        //    this.dateOfBirth = dateOfBirth;
-        //    this.document = document;
-        //    this.address = address;
-        //    this.phoneNumber = phoneNumber;
-        //    this.emailAddress = emailAddress;
-        //}
-
         public override string ToString()
         {
-            return $"CustomerID: {customerID}\nFirst Name: {firstName}\nLast Name: {lastName}\nDate Of Birth: {dateOfBirth}\nDocument:{document}\nAddress: {address}\nPhone Number: {phoneNumber}\nEmail: {emailAddress}";
+            string _result = $"CustomerId: {CustomerId}\n";
+            _result += $"FirstName: {FirstName}\n";
+            _result += $"LastName: {LastName}\n";
+            _result += $"DateOfBirth: {DateOfBirth}\n";
+            _result += $"Document: {Document}\n";
+            _result += $"Address: {Address}\n";
+            _result += $"Phone Number: {PhoneNumber}\n";
+            _result += $"Email: {EmailAddress}";
+            return _result;
         }
+
+
         public string DetailsAllMyAccounts()
         {
             string _result = string.Empty;
@@ -66,55 +67,63 @@ namespace BankProject.Classes
     public class ClassDocument
     {
 
-        string documentType { get; set; }
+        string DocumentType { get; set; }
+        string DocumentNumber { get; set; }
+        DateOnly DocumentIssuedDate { get; set; }
+        DateOnly DocumentExpirationDate { get; set; }
 
-        string documentNumber { get; set; }
-
-        DateOnly documentIssuedDate { get; set; }
-
-        DateOnly documentExpirationDate { get; set; }
 
         public ClassDocument(string documentType, string documentNumber, DateOnly documentIssuedDate, DateOnly documentExpirationDate)
         {
-            this.documentType = documentType;
-            this.documentNumber = documentNumber;
-            this.documentIssuedDate = documentIssuedDate;
-            this.documentExpirationDate = documentExpirationDate;
+            DocumentType = documentType;
+            DocumentNumber = documentNumber;
+            DocumentIssuedDate = documentIssuedDate;
+            DocumentExpirationDate = documentExpirationDate;
         }
+
+
         public override string ToString()
         {
-            return $"\nDocument Type: {documentType}\ndocumentNumber: {documentNumber}\ndocumentIssuedDate: {documentIssuedDate}\n Expiration Date: {documentExpirationDate}";
+            string _result = $"\nDocumentType: {DocumentType}\n";
+            _result += $"DocumentNumber: {DocumentNumber}\n";
+            _result += $"DocumentIssuedDate: {DocumentIssuedDate}\n";
+            _result += $"ExpirationDate: {DocumentExpirationDate}\n";
+            return _result;
         }
     }
+
+
 
     public class ClassAddress
     {
 
-        string zipCode { get; set; }
+        string ZipCode { get; set; }
+        string Line1 { get; set; }
+        string Line2 { get; set; }
+        string City { get; set; }
+        string Province { get; set; }
+        string Country { get; set; }
 
-        string line1 { get; set; }
-
-        string line2 { get; set; }
-
-        string city { get; set; }
-
-        string province { get; set; }
-
-        string country { get; set; }
 
         public ClassAddress(string zipCode, string line1, string line2, string city, string province, string country)
         {
-            this.zipCode = zipCode;
-            this.line1 = line1;
-            this.line2 = line2;
-            this.city = city;
-            this.province = province;
-            this.country = country;
+            ZipCode = zipCode;
+            Line1 = line1;
+            Line2 = line2;
+            City = city;
+            Province = province;
+            Country = country;
         }
 
         public override string ToString()
         {
-            return $"\nZip Code: {zipCode}\n{line1}\n{line2}\nCity: {city}\nProvince: {province}\nCountry: {country}";
+            string _result = $"\nZipCode: {ZipCode}\n";
+            _result += $"Line1: {Line1}\n";
+            _result += $"Line2: {Line2}\n";
+            _result += $"City: {City}\n";
+            _result += $"Province: {Province}\n";
+            _result += $"Country: {country}\n";
+            return _result;
         }
     }
 }
