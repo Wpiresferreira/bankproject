2b89fc760f7e8a4e344ea701eecb99fc86a6defd
Wpferreira13 <wagnerpiresferreira@gmail.com>
2023-12-13 00:18:38 -0700

    minor adjusts

--- BankProject/obj/Debug/net7.0-windows/BankProject.AssemblyInfo.cs
+++ BankProject/obj/Debug/net7.0-windows/BankProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1fd77a638cefe8a89d6847953cf7a8105cc904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
