e62b446e8e8ead51152fc8d9ec5f14aa11eaec82
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2023-12-15 07:22:17 -0700

    feat: added logic to create new Branch in Model

--- BankProject/obj/Debug/net7.0-windows/BankProject.AssemblyInfo.cs
+++ BankProject/obj/Debug/net7.0-windows/BankProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36301d311723520c5fac63a33188bd980adfe98e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
